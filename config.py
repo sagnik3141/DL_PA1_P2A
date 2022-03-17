e43903f7a192247929f87e0d4f248a21569cdbcc
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-03-17 10:36:46 +0530

    Modified Hyperparameters

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ def get_args():
     # Model Args
     parser.add_argument('--input_dim', type = int, default = 60, help = "Input dimensions of data")
     parser.add_argument('--num_classes', type = int, default = 5, help = "Number of output classes.")
-    parser.add_argument('--num_nodes_h1', type = int, default = 30, help = "Number of nodes in hidden layer 1.")
-    parser.add_argument('--num_nodes_h2', type = int, default = 10, help = "Number of nodes in hidden layer 2.")
+    parser.add_argument('--num_nodes_h1', type = int, default = 45, help = "Number of nodes in hidden layer 1.")
+    parser.add_argument('--num_nodes_h2', type = int, default = 30, help = "Number of nodes in hidden layer 2.")
 
     # Dataset Args
     parser.add_argument('--data_path', type = str, default = './data/single_label_image_dataset/image_data_dim60.txt')
     parser.add_argument('--labels_path', type = str, default = './data/single_label_image_dataset/image_data_labels.txt')
-    parser.add_argument('--val_split',  type = float, default = 0.15, help = "Fraction of validation data.")
-    parser.add_argument('--test_split',  type = float, default = 0.15, help = "Fraction of test data.")
+    parser.add_argument('--val_split',  type = float, default = 0.10, help = "Fraction of validation data.")
+    parser.add_argument('--test_split',  type = float, default = 0.20, help = "Fraction of test data.")
     parser.add_argument('--shuffle_data', type = bool, default = True, help = "If True, shuffle dataset.")
     parser.add_argument('--random_seed', type = int, default = 60)
     parser.add_argument('--batch_size', type = int, default = 1)
@@ -26,8 +26,8 @@ def get_args():
     parser.add_argument('--optim', type = str, default = 'delta', help = "Optimizer to use. Options: delta (SGD)| gen_delta (SGD with momentum) | adam (Adam)")
     parser.add_argument('--lr', type = float, default = 1e-4, help = "Learning Rate")
     parser.add_argument('--momentum', type = float, default = 0.9, help = "Optimizer Momentum")
-    parser.add_argument('--num_epochs', type = int, default = 250, help = "Total number of epochs")
-    parser.add_argument('--patience', type = int, default = 5, help = "Patience for Early Stopping.")
+    parser.add_argument('--num_epochs', type = int, default = 200, help = "Total number of epochs")
+    parser.add_argument('--patience', type = int, default = 10, help = "Patience for Early Stopping.")
 
     # Test Args
     parser.add_argument('--ckpt_best_path', type = str, default = './checkpoints/epoch_200.pt', help = "Path to weights used for testing.")
